ff92be4242237f7df553607ec077e763857653b0
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-07-23 11:19:19 -0500

    Updates

---
+++ precache-manifest.e2023e957b858a5f0951977aa61a8ebc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06596647542de71e62c48bc160002fec",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c92712b7f7a561e771cc",
+    "url": "/React-Portfolio/static/css/main.0b01321b.chunk.css"
+  },
+  {
+    "revision": "21c987091bfc42ee8548",
+    "url": "/React-Portfolio/static/js/2.d300bf37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.d300bf37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92712b7f7a561e771cc",
+    "url": "/React-Portfolio/static/js/main.6f50126c.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  },
+  {
+    "revision": "a30405aa47967c5b9ab46896312df093",
+    "url": "/React-Portfolio/static/media/43069.a30405aa.jpg"
+  },
+  {
+    "revision": "2d2a5a15445518fbabce431f907c3cd5",
+    "url": "/React-Portfolio/static/media/Contact-Me3.2d2a5a15.gif"
+  },
+  {
+    "revision": "03be180d9b1a53c1cd588bccdf3f50e9",
+    "url": "/React-Portfolio/static/media/Eat-Da-Burger.03be180d.PNG"
+  },
+  {
+    "revision": "607bf5ab961c28fd70ff25797a886f73",
+    "url": "/React-Portfolio/static/media/Quarantine-Meals.607bf5ab.PNG"
+  },
+  {
+    "revision": "99ca37e2606c32cbf0b11b91f3474945",
+    "url": "/React-Portfolio/static/media/WeatherApp.99ca37e2.PNG"
+  },
+  {
+    "revision": "0250fd777aaac4afd18be5557521cc4c",
+    "url": "/React-Portfolio/static/media/Write-It-Out.0250fd77.png"
+  },
+  {
+    "revision": "0efacf491c84dae9298546da9f71aaf2",
+    "url": "/React-Portfolio/static/media/background2.0efacf49.png"
+  },
+  {
+    "revision": "0d55924592c531793b3d400466392e02",
+    "url": "/React-Portfolio/static/media/myProfile.0d559245.jpg"
+  }
+]);
